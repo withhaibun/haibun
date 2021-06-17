e88721dcab78beded4ecba7272d23a34c96d2de7
Vid <vid_github@zooid.org>
2021-06-17 15:57:22 -0400

    better organized results

--- src/cli.ts
+++ src/cli.ts
@@ -13,11 +13,9 @@ async function go() {
 
   const specl = getConfigOrDefault(base);
   repl.start().context.runtime = runtime;
-  const { result, shared: sharedOut } = await run({ specl, base, logger: new Logger({ level: process.env.LOG_LEVEL || 'log'}), runtime, featureFilter  });
-  if (!result.ok) {
-    console.info('result', JSON.stringify({ ok: result.ok, failure: result.failure, failed: result.results?.find((r) => !r.ok) }, null, 2), { sharedOut });
-  } else {
-    console.info('result', JSON.stringify({ ok: result.ok }, null, 2), { sharedOut });
+  const { result, shared: sharedOut } = await run({ specl, base, logger: new Logger({ level: process.env.LOG_LEVEL || 'log' }), runtime, featureFilter });
+  console.log(result);
+  if (result.ok) {
     process.exit(0);
   }
 }
