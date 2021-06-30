8947b45b9a6d5f29dad3d74db308522dbd0ee28a
Vid <vid_github@zooid.org>
2021-06-30 09:37:18 -0400

    setting up inter-package references, ignore build

--- packages/server/build/src/lib/defs.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.HAIBUN = exports.OK = void 0;
-exports.OK = { ok: true };
-exports.HAIBUN = 'HAIBUN';
-//# sourceMappingURL=defs.js.map
