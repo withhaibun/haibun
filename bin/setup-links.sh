134330271e492cd03360d75f760e9ff4ce0f8f70
Vid <vid_github@zooid.org>
2021-06-30 14:17:57 -0400

    fix linking

--- bin/setup-links.sh
+++ bin/setup-links.sh
@@ -4,17 +4,20 @@
 
 cd modules &&
 (for i in core web-playwright; do
+  cd $i
+  pwd
   npm link 
   cd ../
 done) && 
 
 (for i in client-playwright frontend out-xunit parse-md server web-playwright; do
   cd $i;
+  pwd
   npm link @haibun/core;
   cd ..;
 done) &&
-cd cli &&
-npm link @haibun/core @haibun/web-playwright &&
+cd cli && pwd ;
+npm link && npm link @haibun/core @haibun/web-playwright &&
 
 cd ../..
 
