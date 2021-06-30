169375b673acf6a77cd21a127f8d9d1230e62938
Vid <vid_github@zooid.org>
2021-06-30 11:21:39 -0400

    more reorganizing

--- packages/playwright-web/build/PlaywrightWeb.d.ts
+++
@@ -1,4 +0,0 @@
-import { IExtensionConstructor } from '@haibun/core/build/lib/defs';
-declare const PlayWrightWeb: IExtensionConstructor;
-export default PlayWrightWeb;
-//# sourceMappingURL=PlaywrightWeb.d.ts.map
--- packages/playwright-web/build/index.d.ts
+++
@@ -1,3 +0,0 @@
-import Web from './PlaywrightWeb';
-export default Web;
-//# sourceMappingURL=index.d.ts.map
