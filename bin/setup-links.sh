4b2828ec9931c09b3753e6c23f6cd33f731b7e65
Vid <vid_github@zooid.org>
2021-07-07 16:40:14 -0400

    move server to web-server-express

--- bin/setup-links.sh
+++ bin/setup-links.sh
@@ -3,21 +3,21 @@
 ## FIXME spent a day trying to get lerna set up properly. Deno, save me.
 
 cd modules &&
-(for i in core web-playwright server; do
+(for i in core web-playwright web-server-express; do
   cd $i
-  pwd
+  echo "\nsetup link for $i"
   npm link 
   cd ../
 done) && 
 
-(for i in client-playwright frontend out-xunit parse-md server web-playwright; do
+(for i in client-playwright frontend out-xunit parse-md web-server-express web-playwright; do
   cd $i;
-  pwd
+  echo "\nlink $i"
   npm link @haibun/core;
   cd ..;
 done) &&
-cd cli && pwd ;
-npm link && npm link @haibun/core @haibun/web-playwright @haibun/server &&
+cd cli ; echo "\nlinking cli" ;
+npm link && npm link @haibun/core @haibun/web-playwright @haibun/web-server-express &&
 
 cd ../..
 
