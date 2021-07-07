dc75787e00f02dff098aea3ef9770370146ed5ad
Vid <vid_github@zooid.org>
2021-07-07 18:30:27 -0400

    add .prettierc to projects

--- modules/web-server-express/src/web-server-stepper-route.test.ts
+++ modules/web-server-express/src/web-server-stepper-route.test.ts
@@ -1,76 +1,45 @@
-import fetch from "node-fetch";
-import { Request, Response } from "express";
+import fetch from 'node-fetch';
+import { Request, Response } from 'express';
 
-import {
-  actionOK,
-  getDefaultWorld,
-  getFromRuntime,
-  getStepper,
-} from "@haibun/core/build/lib/util";
-import { testRun } from "@haibun/core/build/lib/TestSteps";
+import { actionOK, getDefaultWorld, getFromRuntime, getStepper } from '@haibun/core/build/lib/util';
+import { testRun } from '@haibun/core/build/lib/TestSteps';
 
-import server, {
-  CHECK_LISTENER,
-  ICheckListener,
-  IWebServer,
-  IWebServerStepper,
-  WEBSERVER,
-  WEBSERVER_STEPPER,
-} from "./web-server-stepper";
-import {
-  IExtensionConstructor,
-  IStepper,
-  TKeyString,
-  TWorld,
-} from "@haibun/core/src/lib/defs";
+import server, { CHECK_LISTENER, ICheckListener, IWebServer, IWebServerStepper, WEBSERVER, WEBSERVER_STEPPER } from './web-server-stepper';
+import { IExtensionConstructor, IStepper, TKeyString, TWorld } from '@haibun/core/src/lib/defs';
 
-describe("route mount", () => {
-  it("mounts a route", async () => {
-    const TestRoute: IExtensionConstructor = class TestRoute
-      implements IStepper
-    {
+describe('route mount', () => {
+  it('mounts a route', async () => {
+    const TestRoute: IExtensionConstructor = class TestRoute implements IStepper {
       world: TWorld;
       constructor(world: TWorld) {
         this.world = world;
       }
       steps = {
         addRoute: {
-          gwta: "serve test route to {loc}",
+          gwta: 'serve test route to {loc}',
           action: async ({ loc }: TKeyString) => {
-            const route = (req: Request, res: Response) =>
-              res.status(200).send("ok");
-            const webserver: IWebServer = getFromRuntime(
-              this.world.runtime,
-              WEBSERVER
-            );
+            const route = (req: Request, res: Response) => res.status(200).send('ok');
+            const webserver: IWebServer = getFromRuntime(this.world.runtime, WEBSERVER);
 
-            const checkListener = getFromRuntime<ICheckListener>(
-              this.world.runtime,
-              CHECK_LISTENER
-            );
+            const checkListener = getFromRuntime<ICheckListener>(this.world.runtime, CHECK_LISTENER);
             await checkListener(this.world.options, webserver);
-            await webserver.addRoute("get", loc, route);
+            await webserver.addRoute('get', loc, route);
             return actionOK();
           },
         },
       };
     };
     const { world } = getDefaultWorld();
-    const { result, steppers } = await testRun(
-      "/test/route",
-      [server, TestRoute],
-      world,
-      {
-        options: {},
-        extraOptions: {
-          [`HAIBUN_O_${WEBSERVER_STEPPER.toUpperCase()}_PORT`]: "8124",
-        },
-      }
-    );
+    const { result, steppers } = await testRun('/test/route', [server, TestRoute], world, {
+      options: {},
+      extraOptions: {
+        [`HAIBUN_O_${WEBSERVER_STEPPER.toUpperCase()}_PORT`]: '8124',
+      },
+    });
 
     expect(result.ok).toBe(true);
-    const content = await fetch("http://localhost:8124/test");
-    expect(await content.text()).toEqual("ok");
+    const content = await fetch('http://localhost:8124/test');
+    expect(await content.text()).toEqual('ok');
 
     getStepper<IWebServerStepper>(steppers!, WEBSERVER_STEPPER).close();
   });
--- modules/web-server-express/src/web-server-stepper.ts
+++ modules/web-server-express/src/web-server-stepper.ts
@@ -1,38 +1,25 @@
-import {
-  IHasOptions,
-  IStepper,
-  IExtensionConstructor,
-  OK,
-  TWorld,
-  TKeyString,
-  TOptions,
-} from "@haibun/core/build/lib/defs";
-import { actionNotOK } from "@haibun/core/src/lib/util";
-import { RequestHandler } from "express";
-import { ServerExpress, DEFAULT_PORT } from "./server-express";
+import { IHasOptions, IStepper, IExtensionConstructor, OK, TWorld, TKeyString, TOptions } from '@haibun/core/build/lib/defs';
+import { actionNotOK } from '@haibun/core/src/lib/util';
+import { RequestHandler } from 'express';
+import { ServerExpress, DEFAULT_PORT } from './server-express';
 
-export const WEBSERVER = "webserver";
-export const WEBSERVER_STEPPER = "WebServerStepper";
-export const CHECK_LISTENER = "CHECK_LISTENER";
+export const WEBSERVER = 'webserver';
+export const WEBSERVER_STEPPER = 'WebServerStepper';
+export const CHECK_LISTENER = 'CHECK_LISTENER';
 
-const WebServerStepper: IExtensionConstructor = class WebServerStepper
-  implements IStepper, IHasOptions
-{
+const WebServerStepper: IExtensionConstructor = class WebServerStepper implements IStepper, IHasOptions {
   webserver: ServerExpress | undefined;
   world: TWorld;
 
   options = {
     PORT: {
       desc: `change web server port from ${DEFAULT_PORT}`,
-      parse: (port: string) => parseInt(port, 10)
+      parse: (port: string) => parseInt(port, 10),
     },
   };
   constructor(world: TWorld) {
     this.world = world;
-    this.webserver = new ServerExpress(
-      this.world.logger,
-      [process.cwd(), "files"].join("/")
-    );
+    this.webserver = new ServerExpress(this.world.logger, [process.cwd(), 'files'].join('/'));
     this.world.runtime[WEBSERVER] = this.webserver;
     this.world.runtime[CHECK_LISTENER] = WebServerStepper.checkListener;
   }
@@ -42,20 +29,15 @@ const WebServerStepper: IExtensionConstructor = class WebServerStepper
   }
 
   static async checkListener(options: TOptions, webserver: IWebServer) {
-    const port = options[
-      `HAIBUN_O_${WEBSERVER_STEPPER.toUpperCase()}_PORT`
-    ] as number;
+    const port = options[`HAIBUN_O_${WEBSERVER_STEPPER.toUpperCase()}_PORT`] as number;
     await webserver.listening(port || DEFAULT_PORT);
   }
 
   steps = {
     serveFiles: {
-      gwta: "serve files from {loc}",
+      gwta: 'serve files from {loc}',
       action: async ({ loc }: TKeyString) => {
-        await WebServerStepper.checkListener(
-          this.world.options,
-          this.world.runtime[WEBSERVER]
-        );
+        await WebServerStepper.checkListener(this.world.options, this.world.runtime[WEBSERVER]);
         const ws: IWebServer = await this.world.runtime[WEBSERVER];
         const error = await ws.addStaticFolder(loc);
 
@@ -79,4 +61,4 @@ export interface IWebServer {
   addRoute(type: TRouteType, path: string, route: RequestHandler): void;
 }
 
-export type TRouteType = "get";
+export type TRouteType = 'get';
