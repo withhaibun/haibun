5f6fa0aad68a977aa46973d35f1b7bd9a7b1afc0
Vid <vid_github@zooid.org>
2021-07-07 18:30:27 -0400

    add .prettierc to projects

--- modules/web-server-express/src/web-server-stepper.test.ts
+++ modules/web-server-express/src/web-server-stepper.test.ts
@@ -1,43 +1,29 @@
-import fetch from "node-fetch";
+import fetch from 'node-fetch';
 
-import { Executor } from "@haibun/core/build/lib/Executor";
-import {
-  getDefaultWorld,
-  getStepper,
-} from "@haibun/core/build/lib/util";
-import { getTestEnv, testRun } from "@haibun/core/build/lib/TestSteps";
+import { Executor } from '@haibun/core/build/lib/Executor';
+import { getDefaultWorld, getStepper } from '@haibun/core/build/lib/util';
+import { getTestEnv, testRun } from '@haibun/core/build/lib/TestSteps';
 
-import server, {
-  IWebServerStepper,
-  WEBSERVER_STEPPER,
-} from "./web-server-stepper";
+import server, { IWebServerStepper, WEBSERVER_STEPPER } from './web-server-stepper';
 
-const serverLoc = [process.cwd(), "build", "web-server-stepper"].join("/");
+const serverLoc = [process.cwd(), 'build', 'web-server-stepper'].join('/');
 
-describe("static mount", () => {
-  it("listens on serve files", async () => {
-    const { world, vstep, steppers } = await getTestEnv(
-      [serverLoc],
-      "serve files from test",
-      getDefaultWorld().world
-    );
+describe('static mount', () => {
+  it('listens on serve files', async () => {
+    const { world, vstep, steppers } = await getTestEnv([serverLoc], 'serve files from test', getDefaultWorld().world);
     const res = await Executor.doFeatureStep(vstep, world);
 
     expect(res.ok).toBe(true);
     const server: IWebServerStepper = getStepper(steppers, WEBSERVER_STEPPER);
 
     expect(server.webserver).toBeDefined();
-    const content = await fetch("http://localhost:8123/testfile");
-    expect(await content.text()).toEqual("content");
+    const content = await fetch('http://localhost:8123/testfile');
+    expect(await content.text()).toEqual('content');
 
     await server.close();
   });
-  it("restricts characters used in static mount folder name", async () => {
-    const { world, vstep, steppers } = await getTestEnv(
-      [serverLoc],
-      "serve files from l*(*$**",
-      getDefaultWorld().world
-    );
+  it('restricts characters used in static mount folder name', async () => {
+    const { world, vstep, steppers } = await getTestEnv([serverLoc], 'serve files from l*(*$**', getDefaultWorld().world);
 
     const res = await Executor.doFeatureStep(vstep, world);
     expect(res.ok).toBe(false);
@@ -45,21 +31,13 @@ describe("static mount", () => {
     getStepper<IWebServerStepper>(steppers, WEBSERVER_STEPPER).close();
   });
   it("doesn't re-mount same static mount", async () => {
-    const { result, steppers } = await testRun(
-      "/test/static-no-remount",
-      [server],
-      getDefaultWorld().world
-    );
+    const { result, steppers } = await testRun('/test/static-no-remount', [server], getDefaultWorld().world);
 
     expect(result.ok).toBe(true);
     getStepper<IWebServerStepper>(steppers!, WEBSERVER_STEPPER).close();
   });
   it("doesn't permit different static mount", async () => {
-    const { result, steppers } = await testRun(
-      "/test/static-fails",
-      [server],
-      getDefaultWorld().world
-    );
+    const { result, steppers } = await testRun('/test/static-fails', [server], getDefaultWorld().world);
 
     expect(result.ok).toBe(false);
     getStepper<IWebServerStepper>(steppers!, WEBSERVER_STEPPER).close();
