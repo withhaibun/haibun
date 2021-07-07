e1d3f11ee4cecb94d1b9494c636a238093056f76
Vid <vid_github@zooid.org>
2021-07-07 16:40:14 -0400

    move server to web-server-express

---
+++ modules/web-server-express/src/server-express.ts
@@ -0,0 +1,99 @@
+import { statSync, existsSync } from "fs";
+import express, { RequestHandler } from "express";
+
+import { TLogger } from "@haibun/core/build/lib/defs";
+import { IWebServer, TRouteType } from "./web-server-stepper";
+
+export const DEFAULT_PORT = 8123;
+
+export class ServerExpress implements IWebServer {
+  logger: TLogger;
+  static listener: any;
+  static app = express();
+  static mounted: { [named: string]: string } = {};
+  base: string;
+  constructor(logger: TLogger, base: string, port: number = DEFAULT_PORT) {
+    this.logger = logger;
+    this.base = base;
+  }
+
+  async listening(port: number) {
+    if (!ServerExpress.listener) {
+      ServerExpress.listener = await ServerExpress.app.listen(port, () =>
+        this.logger.log(`Server listening on port: ${port}`)
+      );
+    } else {
+      this.logger.log("express already started");
+    }
+  }
+
+  async addRoute(type: TRouteType, path: string, route: RequestHandler) {
+    try {
+      const alreadyMounted = this.checkMountBadOrMounted(
+        path,
+        route.toString()
+      );
+      if (alreadyMounted) {
+        return;
+      }
+    } catch (e: any) {
+      return e.message;
+    }
+    this.logger.log(`serving route from ${path}`);
+
+    await ServerExpress.app[type](path, route);
+  }
+
+  async addStaticFolder(subdir: string): Promise<string | undefined> {
+    const folder = [this.base, subdir].join("/");
+    const loc = "/";
+    try {
+      const alreadyMounted = this.checkMountBadOrMounted(loc, folder);
+      if (alreadyMounted) {
+        return;
+      }
+    } catch (e: any) {
+      return e.message;
+    }
+    if (!existsSync(folder)) {
+      throw Error(`"${folder}" doesn't exist`);
+    }
+    const stat = statSync(folder);
+    if (!stat.isDirectory()) {
+      throw Error(`"${folder}" is not a directory`);
+    }
+
+    ServerExpress.mounted[loc] = folder;
+    this.logger.info(`serving files from ${folder} at ${loc}`);
+    await ServerExpress.app.use(express.static(folder));
+    return;
+  }
+
+  checkMountBadOrMounted(loc: string, what: string): boolean {
+    if (!ServerExpress.listener) {
+      throw Error(`listening must be called before mount`);
+    }
+    if (!loc) {
+      throw Error(`missing mount location`);
+    }
+
+    if (loc !== loc.replace(/[^a-zA-Z-0-9\/]/g, "")) {
+      throw Error(`mount folder ${loc} has illegal characters`);
+    }
+    const alreadyMounted = ServerExpress.mounted[loc];
+    if (alreadyMounted === what) {
+      this.logger.log(`${alreadyMounted} already mounted at ${loc}`);
+      return true;
+    } else if (alreadyMounted && alreadyMounted !== what) {
+      throw Error(
+        `cannot mount ${what} at ${loc}, ${alreadyMounted} is already mounted}`
+      );
+    }
+    return false;
+  }
+
+  async close() {
+    this.logger.info("closing server");
+    await ServerExpress.listener?.close();
+  }
+}
---
+++ modules/web-server-express/src/web-server-stepper.ts
@@ -0,0 +1,82 @@
+import {
+  IHasOptions,
+  IStepper,
+  IExtensionConstructor,
+  OK,
+  TWorld,
+  TKeyString,
+  TOptions,
+} from "@haibun/core/build/lib/defs";
+import { actionNotOK } from "@haibun/core/src/lib/util";
+import { RequestHandler } from "express";
+import { ServerExpress, DEFAULT_PORT } from "./server-express";
+
+export const WEBSERVER = "webserver";
+export const WEBSERVER_STEPPER = "WebServerStepper";
+export const CHECK_LISTENER = "CHECK_LISTENER";
+
+const WebServerStepper: IExtensionConstructor = class WebServerStepper
+  implements IStepper, IHasOptions
+{
+  webserver: ServerExpress | undefined;
+  world: TWorld;
+
+  options = {
+    PORT: {
+      desc: `change web server port from ${DEFAULT_PORT}`,
+      parse: (port: string) => parseInt(port, 10)
+    },
+  };
+  constructor(world: TWorld) {
+    this.world = world;
+    this.webserver = new ServerExpress(
+      this.world.logger,
+      [process.cwd(), "files"].join("/")
+    );
+    this.world.runtime[WEBSERVER] = this.webserver;
+    this.world.runtime[CHECK_LISTENER] = WebServerStepper.checkListener;
+  }
+
+  async close() {
+    await this.webserver?.close();
+  }
+
+  static async checkListener(options: TOptions, webserver: IWebServer) {
+    const port = options[
+      `HAIBUN_O_${WEBSERVER_STEPPER.toUpperCase()}_PORT`
+    ] as number;
+    await webserver.listening(port || DEFAULT_PORT);
+  }
+
+  steps = {
+    serveFiles: {
+      gwta: "serve files from {loc}",
+      action: async ({ loc }: TKeyString) => {
+        await WebServerStepper.checkListener(
+          this.world.options,
+          this.world.runtime[WEBSERVER]
+        );
+        const ws: IWebServer = await this.world.runtime[WEBSERVER];
+        const error = await ws.addStaticFolder(loc);
+
+        return error === undefined ? OK : actionNotOK(error);
+      },
+    },
+  };
+};
+export default WebServerStepper;
+
+export type ICheckListener = (options: TOptions, webserver: IWebServer) => void;
+export interface IWebServerStepper {
+  webserver: IWebServer;
+  close: () => void;
+  checkListener: ICheckListener;
+}
+
+export interface IWebServer {
+  addStaticFolder(subdir: string): Promise<string | undefined>;
+  listening(port: number): void;
+  addRoute(type: TRouteType, path: string, route: RequestHandler): void;
+}
+
+export type TRouteType = "get";
