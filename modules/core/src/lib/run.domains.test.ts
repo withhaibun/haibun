357b1754773fea3023ae28939990cc896fbf5a03
Vid <vid_github@zooid.org>
2021-08-18 16:52:51 -0400

    new components

---
+++ modules/core/src/lib/run.domains.test.ts
@@ -0,0 +1,37 @@
+import { IExtensionConstructor, IStepper, IHasDomains, TWorld } from './defs';
+import { run } from './run';
+import { getOptionsOrDefault, getDefaultWorld, actionOK } from './util';
+
+const TTYPE = 'house';
+
+const TestStepsWithDomain: IExtensionConstructor = class TestStepsWithDomain implements IStepper, IHasDomains {
+  world: TWorld;
+  domains = [
+    { name: TTYPE, fileType: TTYPE, is: 'string', validate: () => undefined },
+    { name: 'door', from: TTYPE, is: 'string' },
+  ];
+  constructor(world: TWorld) {
+    this.world = world;
+  }
+  steps = {
+    test: {
+      exact: 'The door is open',
+      action: async (input: any) => actionOK(),
+    },
+  };
+};
+
+describe('domain object from background', () => {
+  it('domain object', async () => {
+    const base = process.cwd() + '/test/projects/domains/domain-object-from-background';
+    const specl = getOptionsOrDefault(base);
+
+    const world = { ...getDefaultWorld().world };
+    const { result } = await run({ specl, base, addSteppers: [TestStepsWithDomain], world });
+    console.log('###result', result.failure ? result.failure?.error : result, '\nworld', world.shared, '\nD', world.domains[0]);
+
+    expect(result.ok).toBe(true);
+    expect(world.domains[0].shared.get(TTYPE)).toBeDefined();
+    expect(world.domains[0].shared.get(TTYPE).get('door').closed).toEqual('true');
+  });
+});
---
+++ modules/core/test/projects/domains/domain-object-from-background/backgrounds/door.house.feature
@@ -0,0 +1,1 @@
+Set door to open
