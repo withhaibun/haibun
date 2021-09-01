ec41698819dba3eef5727aa54707b13247b199da
Vid <vid_github@zooid.org>
2021-09-01 11:49:53 -0400

    adding conformance and annotation (wip)

--- modules/parse-md/src/parse.ts
+++ modules/parse-md/src/parse.ts
@@ -1,41 +1,43 @@
-import { readFileSync, writeFileSync } from "fs";
-import fetch from "node-fetch";
-import TurndownService from "turndown";
-import { TSpecl, IStepper } from "@haibun/core/build/lib/defs";
+import { readFileSync, writeFileSync } from 'fs';
+import fetch from 'node-fetch';
+import TurndownService from 'turndown';
 
-export async function parse(specl: TSpecl, base: string, steppers: IStepper[]) {
-  const { refs } = specl;
-  const { docs } = refs!;
+export async function parseMatches(docs: string[], base: string, matches: RegExp[]) {
   let conditions = [];
 
-  for (const stepper of steppers) {
-    for (const doc in docs) {
-      let content: string;
-      const loc = `${base}/refs/${doc}.md`;
-      try {
-        content = readFileSync(loc, "utf-8");
-      } catch (e) {
-        console.info(`fetching ${loc} from ${docs[doc].src}`);
-        const response = await fetch(docs[doc].src);
-        content = await response.text();
-        const turndownService = new TurndownService();
-        const markdown = turndownService.turndown(content);
-        writeFileSync(loc, markdown);
-      }
+  for (const doc of docs) {
+    let markdown: string;
+    const loc = `${base}/refs/${doc}.md`;
+    try {
+      markdown = readFileSync(loc, 'utf-8');
+    } catch (e) {
+      const content = await fetchFileOrUri(doc);
+      const turndownService = new TurndownService();
+      markdown = turndownService.turndown(content);
+      writeFileSync(loc, markdown);
+    }
 
-      for (const [name, step] of Object.entries(stepper.steps)) {
-        const matches = content.matchAll(step.match!);
-        for (const match of matches) {
-          const [m] = match;
-          conditions.push({
-            doc,
-            condition: m,
-            index: match.index,
-          });
-        }
+    for (const match of matches) {
+      const matches = markdown.matchAll(match);
+      for (const match of matches) {
+        const [m] = match;
+        conditions.push({
+          doc,
+          condition: m,
+          index: match.index,
+        });
       }
-      writeFileSync(`${base}/features/${doc}.md`, conditions.map(c => c.condition).join('\n'));
+      writeFileSync(`${base}/features/${doc}.md`, conditions.map((c) => c.condition).join('\n'));
     }
     console.info('wrote', Object.keys(docs));
   }
+}
+
+async function fetchFileOrUri(doc: string) {
+  console.info(`fetching ${doc}`);
+  if (doc.includes('://')) {
+    const response = await fetch(doc);
+    return await response.text();
+  }
+  return readFileSync(doc, 'utf-8');
 }
