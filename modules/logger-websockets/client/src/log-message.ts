c994875dd119086fd1ebd94e20042ca81ee70adb
David H. Mason <vid_github@zooid.org>
2021-09-09 19:33:35 -0400

    cleanup

--- modules/logger-websockets/client/src/log-message.ts
+++ modules/logger-websockets/client/src/log-message.ts
@@ -11,15 +11,6 @@ import {
 export default class LogMessage extends LitElement {
   @property({ type: String }) level = '';
   @property({ type: String }) message = '';
-  @property({ type: String }) topic = {};
-
-  static get styles() {
-    return css`
-      p {
-        color: blue;
-      }
-    `;
-  }
 
   render(): TemplateResult {
     return html`<div>${this.level}: ${this.message}</div>`;
