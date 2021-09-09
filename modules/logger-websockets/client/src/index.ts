2b4f5637c78d00b757fc06009413f0983833f231
Vid <vid_github@zooid.org>
2021-09-09 17:21:58 -0400

    interface improvements

--- modules/logger-websockets/client/src/index.ts
+++ modules/logger-websockets/client/src/index.ts
@@ -9,3 +9,6 @@ import './log-messages';
 import './topic-result';
 import './topic-results';
 import './message-processor';
+
+const upload = document.querySelector('vaadin-upload');
+(upload as any).set('i18n.addFiles.many', 'Select SCTM files');
