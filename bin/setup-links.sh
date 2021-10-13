780bbf9cb357781b95a2803d545778030570b196
vid <vid_github@zooid.org>
2021-10-13 19:13:21 +0000

    fix build

--- bin/setup-links.sh
+++ bin/setup-links.sh
@@ -3,21 +3,37 @@
 ## FIXME spent a day trying to get lerna set up properly. Deno, save me.
 
 cd modules &&
-(for i in core web-playwright web-http web-server-express out-xunit; do
+(for i in core ; do
   cd $i
-  echo "\nsetup link for $i"
-  npm link 
+  npm i &&  \
+  tsc -b . && \ 
+  echo "\nsetup link for $i" && \
+  npm link &&  \
   cd ../
-done) && 
+done) &&  \
 
-(for i in client-playwright web-http frontend out-xunit parse-md web-server-express web-playwright; do
+(for i in web-playwright web-http web-server-express out-xunit; do
+  cd $i
+  npm i &&  \
+  npm link @haibun/core && \
+  tsc -b . &&  \
+  echo "\nsetup link for $i" && \
+  npm link &&  \
+  cd ../
+done) &&  \
+
+(for i in client-playwright frontend parse-md; do
   cd $i;
   echo "\nlink $i"
-  npm link @haibun/core;
+  npm link @haibun/core && \
+  tsc -b . && \
   cd ..;
-done) &&
+done) && \
+
 cd cli ; echo "\nlinking cli" ;
-npm link && npm link @haibun/core @haibun/web-playwright @haibun/web-http @haibun/web-server-express &&
+npm link @haibun/core @haibun/web-playwright @haibun/web-http @haibun/web-server-express && \
+tsc -b . && \
+npm link && \
 
 cd ../..
 
