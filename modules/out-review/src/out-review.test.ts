dfff687aab96a4f877aebecb82186e72e2e4ecb2
vid <vid_github@zooid.org>
2021-11-10 10:40:48 -0500

    add review

---
+++ modules/out-review/src/out-review.test.ts
@@ -0,0 +1,7 @@
+const { convert } = require("xmlbuilder2");
+
+import OutReview from "./out-review";
+import { testRun, getDefaultWorld } from "@haibun/core/build/lib/test/lib";
+import TestSteps from "@haibun/core/build/lib/test/TestSteps";
+import { resultOutput } from "@haibun/core/build/lib/util";
+
