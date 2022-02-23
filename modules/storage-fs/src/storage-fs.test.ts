6efaea29090bca39afe6ac3496c9c2e31e579f9b
vid <vid_github@zooid.org>
2022-02-23 16:58:47 -0500

    organizing options, storage and reviews

---
+++ modules/storage-fs/src/storage-fs.test.ts
@@ -0,0 +1,11 @@
+import { getDefaultWorld } from '@haibun/core/build/lib/test/lib';
+import StorageFS from './storage-fs';
+
+describe('getCaptureDir', () => {
+    it('gets capture dir', async () => {
+        const storageFS = new StorageFS();
+        const { world } = getDefaultWorld(0);
+        const dir = await storageFS.getCaptureDir(world, 'test');
+        expect(dir).toEqual('./capture/loop-0/seq-0/featn-0/mem-0/test');
+    });
+});
