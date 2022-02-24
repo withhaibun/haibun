c53e467b777cfed0887ef55d35192ab25056ab4c
vid <vid_github@zooid.org>
2022-02-24 15:11:56 -0500

    rename

--- modules/core/src/steps/haibun.test.ts
+++ modules/core/src/steps/haibun.test.ts
@@ -1,10 +1,10 @@
-import { Executor } from '../phases/Executor';
+import { FeatureExecutor } from '../phases/Executor';
 import { getDefaultWorld, getTestEnv } from '../lib/test/lib';
 
 describe('haibun', () => {
   it('finds prose', async () => {
     const { world, vstep } = await getTestEnv(['haibun'], 'A sentence.', getDefaultWorld(0).world);
-    const res = await Executor.doFeatureStep(vstep, world);
+    const res = await FeatureExecutor.doFeatureStep(vstep, world);
     
     expect(res.ok).toBe(true);
     expect(res.actionResults[0].name).toBe('prose');
