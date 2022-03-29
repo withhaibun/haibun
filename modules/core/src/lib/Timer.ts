e11c3f6af64580da2fd2c877e5f88ed1fae8279b
vid <vid_github@zooid.org>
2022-03-29 11:03:44 -0400

    add setKey method

--- modules/core/src/lib/Timer.ts
+++ modules/core/src/lib/Timer.ts
@@ -1,7 +1,12 @@
 
 export class Timer {
   static startTime = new Date();
+  static key = `${Timer.startTime}`;
   static START_TIME = process.hrtime();
+  static setKey(newKey: string) {
+    Timer.key = newKey;
+  }
+
   since() {
     const [s, ns] = process.hrtime(Timer.START_TIME);
     return s + ns / 1000000000;
