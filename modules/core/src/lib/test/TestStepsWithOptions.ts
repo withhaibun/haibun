b73bda6afba7259c883e518fbd8f067922a383d2
David H. Mason <vid_github@zooid.org>
2023-04-13 15:57:17 -0400

    Politeness tests (#44)
    * add after every
    
    * 'update utils to version 1.24.0'
    
    * 'update core to version 1.24.0'
    
    * 'update web-server-express to version 1.24.0'
    
    * 'update cli to version 1.24.0'
    
    * 'update context to version 1.24.0'
    
    * 'update web-http to version 1.24.0'
    
    * 'update parse-md to version 1.24.0'
    
    * 'update web-playwright to version 1.24.0'
    
    * 'update feature-importer to version 1.24.0'
    
    * 'update domain-webpage to version 1.24.0'
    
    * 'update storage-mem to version 1.24.0'
    
    * 'update out-review to version 1.24.0'
    
    * 'update domain-storage to version 1.24.0'
    
    * 'update storage-fs to version 1.24.0'
    
    * 'update out-xunit to version 1.24.0'
    
    * 'update browser-extension to version 1.24.0'
    
    * 'update logger-websockets to version 1.24.0'
    
    * 'update feature-recorder to version 1.24.0'
    
    * 'update utils to version 1.24.1'
    
    * 'update core to version 1.24.1'
    
    * 'update web-server-express to version 1.24.1'
    
    * 'update cli to version 1.24.1'
    
    * 'update context to version 1.24.1'
    
    * 'update web-http to version 1.24.1'
    
    * 'update parse-md to version 1.24.1'
    
    * 'update web-playwright to version 1.24.1'
    
    * 'update feature-importer to version 1.24.1'
    
    * 'update domain-webpage to version 1.24.1'
    
    * 'update storage-mem to version 1.24.1'
    
    * 'update out-review to version 1.24.1'
    
    * 'update domain-storage to version 1.24.1'
    
    * 'update storage-fs to version 1.24.1'
    
    * 'update out-xunit to version 1.24.1'
    
    * 'update browser-extension to version 1.24.1'
    
    * 'update logger-websockets to version 1.24.1'
    
    * 'update feature-recorder to version 1.24.1'
    
    * update tests
    
    * rework events

--- modules/core/src/lib/test/TestStepsWithOptions.ts
+++ modules/core/src/lib/test/TestStepsWithOptions.ts
@@ -1,7 +1,7 @@
 import { IHasOptions, AStepper } from '../defs.js';
 import { actionOK, getStepperOption } from '../util/index.js';
 
-const TestStepsWithOptions = class TestStepsWithOptions extends AStepper implements IHasOptions {
+export const TestStepsWithOptions = class TestStepsWithOptions extends AStepper implements IHasOptions {
   options = {
     EXISTS: {
       desc: 'option exists',
@@ -10,7 +10,7 @@ const TestStepsWithOptions = class TestStepsWithOptions extends AStepper impleme
   };
   steps = {
     test: {
-      exact: 'When I have a stepper option',
+      exact: 'have a stepper option',
       action: async () => {
         const res = getStepperOption(this, 'EXISTS', this.getWorld().extraOptions);
         return actionOK({ options: { summary: 'options', details: res } });
@@ -20,3 +20,22 @@ const TestStepsWithOptions = class TestStepsWithOptions extends AStepper impleme
 };
 
 export default TestStepsWithOptions;
+
+export const TestStepsWithRequiredOptions = class TestStepsWithRequiredOptions extends AStepper implements IHasOptions {
+  options = {
+    EXISTS: {
+      required: true,
+      desc: 'option exists',
+      parse: (input: string) => ({ result: 42 }),
+    },
+  };
+  steps = {
+    test: {
+      exact: 'have a stepper option',
+      action: async () => {
+        const res = getStepperOption(this, 'EXISTS', this.getWorld().extraOptions);
+        return actionOK({ options: { summary: 'options', details: res } });
+      },
+    },
+  };
+};
