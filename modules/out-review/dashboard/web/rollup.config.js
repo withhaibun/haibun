9391a89ac98918e130b6938cc6fc5e85a032c043
Vid <vid_github@zooid.org>
2023-10-25 06:14:57 -0400

    add rollup-plugin-lit-css and reviews.css

--- modules/out-review/dashboard/web/rollup.config.js
+++ modules/out-review/dashboard/web/rollup.config.js
@@ -1,18 +1,19 @@
 import typescript from '@rollup/plugin-typescript';
 import resolve from '@rollup/plugin-node-resolve';
+import litcss from 'rollup-plugin-lit-css';
 
 const dashboard = {
   input: [`./src/dashboard/index.ts`, `./src/dashboard/indexer.ts`],
   output: {
     dir: `built/dashboard/`,
   },
-  plugins: [resolve(), typescript({ outDir: './built/dashboard' })],
+  plugins: [litcss(), resolve(), typescript({ outDir: './built/dashboard' })],
 };
 
 const reviewer = {
   input: `./src/reviews/index.ts`,
   output: { dir: `built/reviewer/` },
-  plugins: [resolve(), typescript({ outDir: './built/reviewer' })],
+  plugins: [litcss(), resolve(), typescript({ outDir: './built/reviewer' })],
 };
 
 export default [dashboard, reviewer];
