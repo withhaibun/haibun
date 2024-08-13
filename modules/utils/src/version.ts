dfdd659c4f41ea068e5224e00de7c2db67d1830f
vid <vid_github@zooid.org>
2024-08-13 14:44:57 -0400

    streamline versioning

--- modules/utils/src/version.ts
+++ modules/utils/src/version.ts
@@ -4,112 +4,140 @@ import { spawn } from './util/index.js';
 const [, me, version, ...extra] = process.argv;
 
 class Versioner {
-  toPublish: string[] = [];
-  version: string;
-  haibun: { [dep: string]: string } = {};
-  constructor(version: string) {
-    this.version = version;
-  }
-  doVersion() {
-    try {
-      const hpkg = JSON.parse(readFileSync('./package.json', 'utf-8'));
-      if (hpkg.name !== 'haibun') {
-        throw Error('not in haibun root');
-      }
-      for (const [dep, version] of Object.entries({
-        ...hpkg.dependencies,
-        ...hpkg.devDependencies,
-      })) {
-        this.haibun[dep] = <string>version;
-      }
-    } catch (e) {
-      console.error(e);
-      process.exit(1);
-    }
+	localAndExtraModules = new Set<{ [name: string]: string }>();
 
-    if (!this.version) {
-      console.error(`usage: ${me}: <version> <extra modules>`);
-      process.exit(1);
-    }
+	version: string;
+	haibunPackageVersions: { [dep: string]: string } = {};
+	constructor(version: string) {
+		if (!version) {
+			console.error(`usage: ${me}: <version> <extra modules>`);
+		}
+		this.version = version;
+	}
+	doVersion() {
+		const haibunPackageJson = this.updateHaibunPackageVersions();
+		haibunPackageJson.version = this.version;
+		writeFileSync('./package.json', JSON.stringify(haibunPackageJson, null, 2));
 
-    for (const [dest, ext] of Object.entries({ src: 'ts', build: 'js' })) {
-      writeFileSync(`./modules/core/${dest}/currentVersion.${ext}`, `export const currentVersion = '${this.version}';\n`);
-    }
+		this.setLocalAndExtraModules();
 
-    const modules = JSON.parse(readFileSync(`./modules/tsconfig.json`, 'utf-8'))
-      .references
-      .map(f => `./modules/${f.path}`)
-      .concat(extra);
+		this.forLocalAndExtraModules(this.updateModule);
+		this.forLocalAndExtraModules(this.npmInstall);
+		this.forLocalAndExtraModules(this.npmTest);
+		this.forLocalAndExtraModules(this.gitCommit);
+		this.forLocalAndExtraModules(this.npmPublish);
+		this.forLocalAndExtraModules(this.gitPush);
 
-    for (const module of modules) {
-      const name = module.replace(/\/$/, '').replace(/.*\//, '');
-      if (this.verifyShouldPublishStructureAndUpdateVersion(name, module)) {
-        this.toPublish.push(module);
-      }
-    }
-    this.verifyShouldPublishStructureAndUpdateVersion('haibun', '.');
-    this.publishAll();
-    const packageJson = JSON.parse(readFileSync('./package.json', 'utf-8'));
-    packageJson.version = this.version;
-    writeFileSync('./package.json', JSON.stringify(packageJson, null, 2));
-    spawn(['git', 'commit', '-m', `'update haibun to version ${this.version}'`, 'package.json', 'modules/core/src/currentVersion.ts'], '.');
-  }
+		this.updateSourceCurrentVersion();
 
-  publishAll() {
-    for (const module of this.toPublish) {
-      console.info('\n\n*** publishing', module);
-      spawn(['npm', 'publish'], module);
-      spawn(['git', 'push'], module);
-    }
-  }
+		this.gitCommit('haibun', '.', ['modules/core/src/currentVersion.ts']);
+	}
 
-  verifyShouldPublishStructureAndUpdateVersion(name: string, location: string) {
-    if (location !== '.') {
-      const pkgFile = `${location}/package.json`;
-      const pkg = JSON.parse(readFileSync(pkgFile, 'utf-8'));
-      if (!pkg.publsh && pkg.publish !== undefined) {
-        return false;
-      }
-      console.info('updating', name);
-      const { main } = pkg;
-      if (main && !main.includes('*') && !existsSync(`${location}/${main}`)) {
-        throw Error(`main file ${main} does not exist in ${location}`);
-      }
-      pkg.version = this.version;
-      for (const d in pkg.dependencies) {
-        if (d.startsWith('@haibun/')) {
-          pkg.dependencies[d] = this.version;
-        }
-        if (this.haibun[d]) {
-          pkg.dependencies[d] = this.haibun[d];
-        }
-      }
-      for (const d in pkg.devDependencies) {
-        if (d.startsWith('@haibun/')) {
-          pkg.devDependencies[d] = this.version;
-        }
-        if (this.haibun[d]) {
-          pkg.devDependencies[d] = this.haibun[d];
-        }
-      }
+	forLocalAndExtraModules(updateModule: (name: string, location: string) => void) {
+		for (const { name, module } of this.localAndExtraModules) {
+			updateModule(name, module);
+		}
+	}
+	updateHaibunPackageVersions() {
+		const hpkg = JSON.parse(readFileSync('./package.json', 'utf-8'));
+		if (hpkg.name !== 'haibun') {
+			throw Error('not in haibun root');
+		}
+		for (const [dep, version] of Object.entries({ ...hpkg.dependencies, ...hpkg.devDependencies })) {
+			this.haibunPackageVersions[dep] = <string>version;
+		}
+		return hpkg;
+	}
 
-      writeFileSync(pkgFile, JSON.stringify(pkg, null, 2));
-      try {
-        // spawn(['npm', 'run', 'test'], location, { env: { NODE_OPTIONS: '--experimental-vm-modules' } });
-      } catch (e) {
-        console.error(`npm test failed for ${name}: ${e}`);
-        throw e;
-      }
+	private updateSourceCurrentVersion() {
+		for (const [dest, ext] of Object.entries({ src: 'ts', build: 'js' })) {
+			writeFileSync(`./modules/core/${dest}/currentVersion.${ext}`, `export const currentVersion = '${this.version}';\n`);
+		}
+	}
 
-      try {
-        spawn(['git', 'commit', '-m', `'update ${name} to version ${this.version}'`, 'package.json',], location);
-      } catch (e) {
-        console.error(`git commit failed for ${name}: ${e}`);
-        throw e;
-      }
-    }
-    return true;
-  }
+	setLocalAndExtraModules() {
+		const modules = JSON.parse(readFileSync(`./modules/tsconfig.json`, 'utf-8'))
+			.references.map((f) => `./modules/${f.path}`)
+			.concat(extra);
+		for (const module in modules) {
+			const name = module.replace(/\/$/, '').replace(/.*\//, '');
+			this.localAndExtraModules.add({ [name]: module });
+		}
+	}
+
+	testAll() {
+		for (const { name, module } of this.localAndExtraModules) {
+			console.info('\n\n*** publishing', module);
+			this.npmTest(name, module);
+		}
+	}
+
+	npmPublish(name, module) {
+		console.info('\n\n*** publishing', module);
+		spawn(['npm', 'publish'], module);
+	}
+	gitPush(name, module) {
+		console.info('\n\n*** publishing', module);
+		spawn(['git', 'push'], module);
+	}
+
+	updateModule(name: string, location: string) {
+		const pkgFile = `${location}/package.json`;
+		const pkg = JSON.parse(readFileSync(pkgFile, 'utf-8'));
+		if (!pkg.publsh && pkg.publish !== undefined) {
+			return false;
+		}
+		console.info('updating', name);
+		const { main } = pkg;
+		if (main && !main.includes('*') && !existsSync(`${location}/${main}`)) {
+			throw Error(`main file ${main} does not exist in ${location}`);
+		}
+		pkg.version = this.version;
+		for (const d in pkg.dependencies) {
+			if (d.startsWith('@haibun/')) {
+				pkg.dependencies[d] = this.version;
+			}
+			if (this.haibunPackageVersions[d]) {
+				pkg.dependencies[d] = this.haibunPackageVersions[d];
+			}
+		}
+		for (const d in pkg.devDependencies) {
+			if (d.startsWith('@haibun/')) {
+				pkg.devDependencies[d] = this.version;
+			}
+			if (this.haibunPackageVersions[d]) {
+				pkg.devDependencies[d] = this.haibunPackageVersions[d];
+			}
+		}
+
+		writeFileSync(pkgFile, JSON.stringify(pkg, null, 2));
+	}
+
+	gitCommit(name: string, location: string, extraPackages = []) {
+		const packages = [...extraPackages, 'package.json'];
+		try {
+			spawn(['git', 'commit', '-m', `'update ${name} to version ${this.version}'`, packages.join(' ')], location);
+		} catch (e) {
+			console.error(`git commit failed for ${name}: ${e}`);
+			throw e;
+		}
+	}
+	npmTest(name: string, location: string) {
+		try {
+			spawn(['npm', 'run', 'test'], location);
+		} catch (e) {
+			console.error(`npm test failed for ${name}: ${e}`);
+			throw e;
+		}
+	}
+	npmInstall(name: string, location: string) {
+		try {
+			spawn(['npm', 'install'], location);
+		} catch (e) {
+			console.error(`npm install failed for ${name}: ${e}`);
+			throw e;
+		}
+	}
 }
 
 new Versioner(version).doVersion();
