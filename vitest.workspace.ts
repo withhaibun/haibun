84673f7ac06c23dc5681eed856ef385fcc866f23
vid <vid_github@zooid.org>
2024-12-30 12:30:03 -0800

    use explicit packages, typed config

--- vitest.workspace.ts
+++ vitest.workspace.ts
@@ -1,1 +1,6 @@
-export default ['modules/*'];
+import { readdirSync, statSync } from 'fs';
+
+const modules = readdirSync('./modules/')
+	.map((d) => `./modules/${d}`)
+	.filter((f) => statSync(f).isDirectory());
+export default modules;
