36db7ea5be727a6c0109386a5b2af7513b4a6a60
vid <vid_github@zooid.org>
2025-03-17 07:59:53 -0700

    rest methods (wip)

--- modules/web-playwright/src/BrowserFactory.ts
+++ modules/web-playwright/src/BrowserFactory.ts
@@ -1,4 +1,3 @@
-
 import { Browser, BrowserContext, Page, chromium, firefox, webkit, BrowserType, devices } from 'playwright';
 
 import { ILogger } from '@haibun/core/build/lib/interfaces/logger.js';
@@ -6,143 +5,153 @@ import { TTag, TTagValue } from '@haibun/core/build/lib/defs.js';
 import { PlaywrightEvents } from './PlaywrightEvents.js';
 
 export const BROWSERS: { [name: string]: BrowserType } = {
-  firefox,
-  chromium,
-  webkit,
+	firefox,
+	chromium,
+	webkit,
 };
 export type TBrowserTypes = 'firefox' | 'chromium' | 'webkit';
 
 export type TBrowserFactoryOptions = {
-  browser: {
-    headless: boolean,
-    devtools?: boolean,
-    args?: string[]
-  },
-  recordVideo?: {
-    dir: string,
-    size: { width: 640, height: 480 },
-  }
-  defaultTimeout?: number,
-  persistentDirectory?: boolean,
-  type?: TBrowserTypes,
-  device?: string
-}
+	browser: {
+		headless: boolean;
+		devtools?: boolean;
+		args?: string[];
+	};
+	recordVideo?: {
+		dir: string;
+		size: { width: 640; height: 480 };
+	};
+	defaultTimeout?: number;
+	persistentDirectory?: boolean;
+	type?: TBrowserTypes;
+	device?: string;
+};
 
 export const DEFAULT_CONFIG_TAG = '_default';
 
 export type PageInstance = Page & { _guid: string };
 
 export class BrowserFactory {
-  static browsers: { [name: string]: Browser } = {};
-  tracers: { [name: string]: PlaywrightEvents } = {};
-  contexts: { [name: string]: BrowserContext } = {};
-  pages: { [name: string]: Page | undefined } = {};
-  static tracer?: PlaywrightEvents = undefined;
-  static configs: {
-    [name: string]: {
-      options: TBrowserFactoryOptions,
-      browserType: BrowserType
-    }
-  } = {};
-  myBrowsers: { [name: string]: Browser; };
+	static browsers: { [name: string]: Browser } = {};
+	tracers: { [name: string]: PlaywrightEvents } = {};
+	browserContexts: { [name: string]: BrowserContext } = {};
+	pages: { [name: string]: Page | undefined } = {};
+	static tracer?: PlaywrightEvents = undefined;
+	static configs: {
+		[name: string]: {
+			options: TBrowserFactoryOptions;
+			browserType: BrowserType;
+		};
+	} = {};
+	static persistentDirectory = undefined;
 
-  private constructor(private logger: ILogger) { }
+	private constructor(private logger: ILogger) {}
 
-  static async getBrowserFactory(logger: ILogger, options: TBrowserFactoryOptions, tag = DEFAULT_CONFIG_TAG) {
-    options.type = options.type || 'chromium';
-    options.device = options.device || '';
+	static async getBrowserFactory(logger: ILogger, options: TBrowserFactoryOptions, tag = DEFAULT_CONFIG_TAG) {
+		options.type = options.type || 'chromium';
+		options.device = options.device || '';
 
-    if (!BROWSERS[options.type]) {
-      throw Error(`browserType not recognized ${options.type}`);
-    }
-    BrowserFactory.configs[tag] = { options, browserType: BROWSERS[options.type] };
-    return new BrowserFactory(logger);
-  }
+		if (!BROWSERS[options.type]) {
+			throw Error(`browserType not recognized ${options.type}`);
+		}
+		BrowserFactory.configs[tag] = { options, browserType: BROWSERS[options.type] };
+		return new BrowserFactory(logger);
+	}
 
-  async getBrowser(type: string, tag = DEFAULT_CONFIG_TAG): Promise<Browser> {
-    if (!BrowserFactory.browsers[type]) {
-      BrowserFactory.browsers[type] = await BrowserFactory.configs[tag].browserType
-        .launch(BrowserFactory.configs[tag].options.browser);
-      this.logger.info(`launched new ${type} browser`);
-    }
-    return BrowserFactory.browsers[type];
-  }
+	async getBrowser(type: string, tag = DEFAULT_CONFIG_TAG): Promise<Browser> {
+		if (!BrowserFactory.browsers[type]) {
+			BrowserFactory.browsers[type] = await BrowserFactory.configs[tag].browserType.launch(
+				BrowserFactory.configs[tag].options.browser
+			);
+			this.logger.info(`launched new ${type} browser`);
+		}
+		return BrowserFactory.browsers[type];
+	}
 
-  getExistingContext({ sequence }: { sequence: TTagValue }) {
-    if (this.contexts[sequence]) {
-      return this.contexts[sequence];
-    }
-  }
+	getExistingBrowserContext({ sequence }: { sequence: TTagValue }) {
+		if (this.browserContexts[sequence]) {
+			return this.browserContexts[sequence];
+		}
+	}
 
-  private async getBrowserContext(sequence: TTagValue, tag = DEFAULT_CONFIG_TAG): Promise<BrowserContext> {
-    if (!this.contexts[sequence]) {
-      let context: BrowserContext;
-      if (BrowserFactory.configs.persistentDirectory) {
-        this.logger.info(`creating new persistent context ${sequence} ${BrowserFactory.configs[tag].options.type}, ${BrowserFactory.configs.persistentDirectory} with ${JSON.stringify(BrowserFactory.configs)}`);
-        context = await BrowserFactory.configs[tag].browserType.launchPersistentContext("", BrowserFactory.configs[tag].options);
-      } else {
-        this.logger.info(`creating new context ${sequence} ${BrowserFactory.configs[tag].options.type}`);
-        const browser = await this.getBrowser(BrowserFactory.configs[tag].options.type);
-        const deviceContext = BrowserFactory.configs[tag].options.device ? { ...devices[BrowserFactory.configs[tag].options.device] } : {};
-        context = await browser.newContext({ ...deviceContext, ...BrowserFactory.configs[tag].options });
-      }
-      this.contexts[sequence] = context;
-      if (BrowserFactory.configs.defaultTimeout) {
-        this.contexts[sequence].setDefaultTimeout(BrowserFactory.configs[tag].options.defaultTimeout)
-      }
-    }
-    return this.contexts[sequence];
-  }
+	private async getBrowserContext(sequence: TTagValue, tag = DEFAULT_CONFIG_TAG): Promise<BrowserContext> {
+		if (!this.browserContexts[sequence]) {
+			let browserContext: BrowserContext;
+			if (BrowserFactory.configs.persistentDirectory) {
+				this.logger.info(
+					`creating new persistent context ${sequence} ${BrowserFactory.configs[tag].options.type}, ${
+						BrowserFactory.configs.persistentDirectory
+					} with ${JSON.stringify(BrowserFactory.configs)}`
+				);
+				browserContext = await BrowserFactory.configs[tag].browserType.launchPersistentContext(
+					'',
+					BrowserFactory.configs[tag].options
+				);
+			} else {
+				this.logger.info(`creating new context ${sequence} ${BrowserFactory.configs[tag].options.type}`);
+				const browser = await this.getBrowser(BrowserFactory.configs[tag].options.type);
+				const deviceContext = BrowserFactory.configs[tag].options.device
+					? { ...devices[BrowserFactory.configs[tag].options.device] }
+					: {};
+				browserContext = await browser.newContext({ ...deviceContext, ...BrowserFactory.configs[tag].options });
+			}
+			this.browserContexts[sequence] = browserContext;
+			if (BrowserFactory.configs.defaultTimeout) {
+				this.browserContexts[sequence].setDefaultTimeout(BrowserFactory.configs[tag].options.defaultTimeout);
+			}
+		}
+		return this.browserContexts[sequence];
+	}
 
-  async closeContext({ sequence }: { sequence: TTagValue }) {
-    if (this.contexts[sequence] !== undefined) {
-      const p = this.pages[sequence];
-      await p && p?.close();
-    }
-    await this.contexts[sequence]?.close();
-    this.tracers[sequence]?.close();
-    delete this.pages[sequence];
-    delete this.contexts[sequence];
-  }
+	async closeContext({ sequence }: { sequence: TTagValue }) {
+		if (this.browserContexts[sequence] !== undefined) {
+			const p = this.pages[sequence];
+			(await p) && p?.close();
+		}
+		await this.browserContexts[sequence]?.close();
+		this.tracers[sequence]?.close();
+		delete this.pages[sequence];
+		delete this.browserContexts[sequence];
+	}
 
-  static async closeBrowsers() {
-    for (const b in BrowserFactory.browsers) {
-      await BrowserFactory.browsers[b].close();
-    }
-  }
-  async close() {
-    await BrowserFactory.closeBrowsers();
-  }
+	static async closeBrowsers() {
+		for (const b in BrowserFactory.browsers) {
+			await BrowserFactory.browsers[b].close();
+		}
+	}
+	async close() {
+		await BrowserFactory.closeBrowsers();
+	}
 
-  pageKey(sequence: number, tab?: number) {
-    return `${sequence}-${tab}`;
-  }
+	pageKey(sequence: number, tab?: number) {
+		return `${sequence}-${tab}`;
+	}
 
-  hasPage({ sequence }: { sequence: TTagValue }, tab?: number) {
-    return !!this.pages[this.pageKey(sequence, tab)]
-  }
+	hasPage({ sequence }: { sequence: TTagValue }, tab?: number) {
+		return !!this.pages[this.pageKey(sequence, tab)];
+	}
 
-  registerPopup({ sequence }: { sequence: TTagValue }, tab: number, popup: Page) {
-    const tt = this.pageKey(sequence, tab);
-    this.pages[tt] = popup;
-  }
+	registerPopup({ sequence }: { sequence: TTagValue }, tab: number, popup: Page) {
+		const tt = this.pageKey(sequence, tab);
+		this.pages[tt] = popup;
+	}
 
-  async getBrowserContextPage(tag: TTag, tab: number): Promise<Page> {
-    const { sequence } = tag;
-    const pageKey = this.pageKey(sequence, tab);
-    let page = this.pages[pageKey];
-    if (page) {
-      await page.bringToFront();
-      return page;
-    }
-    this.logger.info(`creating new page for ${sequence}`);
+	async getBrowserContextPage(tag: TTag, tab: number): Promise<Page> {
+		const { sequence } = tag;
+		const pageKey = this.pageKey(sequence, tab);
+		let page = this.pages[pageKey];
+		if (page) {
+			await page.bringToFront();
+			return page;
+		}
+		this.logger.info(`creating new page for ${sequence}`);
 
-    const context = await this.getBrowserContext(sequence);
-    page = await context.newPage();
-    const tracer = new PlaywrightEvents(this.logger, page, tag);
+		const context = await this.getBrowserContext(sequence);
+		page = await context.newPage();
+		const tracer = new PlaywrightEvents(this.logger, page, tag);
 
-    this.pages[pageKey] = page;
-    this.tracers[sequence] = tracer;
-    return page;
-  }
+		this.pages[pageKey] = page;
+		this.tracers[sequence] = tracer;
+		return page;
+	}
 }
--- modules/web-playwright/src/rest-playwright.ts
+++ modules/web-playwright/src/rest-playwright.ts
@@ -1,25 +1,40 @@
-// haibun-plugin-rest.ts
-import { OK, TNamed } from '@haibun/core/build/lib/defs.js';
+import { Page, Response } from 'playwright';
+
 import { actionNotOK } from '@haibun/core/build/lib/util/index.js';
 import WebPlaywright from './web-playwright.js';
-import { Page } from 'playwright';
+import { TNamed, OK } from '@haibun/core/build/lib/defs.js';
 
-const LAST_REST_RESPONSE = 'lastRestResponse';
+const LAST_REST_RESPONSE = 'LAST_REST_RESPONSE';
 const PAYLOAD_METHODS = ['post', 'put', 'patch'];
 const NO_PAYLOAD_METHODS = ['get', 'delete', 'head'];
 
 export const restSteps = (webPlaywright: WebPlaywright) => ({
+	addAuthBearerToken: {
+		gwta: 'add auth bearer token {token}',
+		action: async ({ token }: TNamed) => {
+			const browserContext = await webPlaywright.getBrowserContext();
+			browserContext.setExtraHTTPHeaders({ Authorization: `Bearer ${token}` });
+			return OK;
+		},
+	},
 	restEndpointRequest: {
-		gwta: 'send {method} to {endpoint}',
+		gwta: 'make an http {method} to {endpoint}',
 		action: async ({ method, endpoint }: TNamed) => {
+			method = method.toLowerCase();
 			if (!NO_PAYLOAD_METHODS.includes(method)) {
 				return actionNotOK(`Method ${method} not supported`);
 			}
 			try {
-				const response = await webPlaywright.withPage<Response>(async (page: Page) => await page[method](endpoint));
-				const responseBody = await response.json();
+				const response = await webPlaywright.withPage<Response>(
+					async (page: Page) =>
+						await page.request[method](endpoint, {
+							// headers: {
+							// 	Authorization: `Bearer testtoken`,
+							// },
+						})
+				);
 
-				webPlaywright.getWorld().shared.set(LAST_REST_RESPONSE, { status: response.status, body: responseBody });
+				webPlaywright.getWorld().shared.set(LAST_REST_RESPONSE, await capturedResponse(response));
 
 				return OK;
 			} catch (error: any) {
@@ -28,8 +43,9 @@ export const restSteps = (webPlaywright: WebPlaywright) => ({
 		},
 	},
 	restEndpointRequestWithPayload: {
-		gwta: 'send {method} to {endpoint} with {payload?}',
+		gwta: 'make an http {method} to {endpoint} with {payload}',
 		action: async ({ method, endpoint, payload }: TNamed) => {
+			method = method.toLowerCase();
 			if (!PAYLOAD_METHODS.includes(method)) {
 				return actionNotOK(`Method ${method} does not support payload`);
 			}
@@ -37,10 +53,11 @@ export const restSteps = (webPlaywright: WebPlaywright) => ({
 				const data = typeof payload === 'object' ? payload : JSON.parse(payload);
 				const requestOptions = { data };
 
-				const response = await webPlaywright.withPage<Response>(async (page: Page) => await page[method](endpoint, requestOptions));
-				const responseBody = await response.json();
+				const response = await webPlaywright.withPage<Response>(
+					async (page: Page) => await page.request[method](endpoint, requestOptions)
+				);
 
-				webPlaywright.getWorld().shared.set(LAST_REST_RESPONSE, { status: response.status, body: responseBody });
+				webPlaywright.getWorld().shared.set(LAST_REST_RESPONSE, await capturedResponse(response));
 
 				return OK;
 			} catch (error: any) {
@@ -49,7 +66,7 @@ export const restSteps = (webPlaywright: WebPlaywright) => ({
 		},
 	},
 	restLastStatusIs: {
-		gwta: 'last status is {status}',
+		gwta: 'http status is {status}',
 		action: async ({ status }: TNamed) => {
 			const response = webPlaywright.getWorld().shared.get(LAST_REST_RESPONSE);
 			if (response && response.status === parseInt(status)) {
@@ -59,23 +76,41 @@ export const restSteps = (webPlaywright: WebPlaywright) => ({
 		},
 	},
 	restResponsePropertyIs: {
-		gwta: 'response property {property} is {value}',
+		gwta: 'http response property {property} is {value}',
 		action: async ({ property, value }: TNamed) => {
 			const response = webPlaywright.getWorld().shared.get(LAST_REST_RESPONSE);
 			if (response && response.body && response.body[property]?.toString() === value) {
 				return OK;
 			}
-			return actionNotOK(`Expected response.${property} to be ${value}, got ${response?.body[property] || 'no response'}`);
+			return actionNotOK(`Expected response.${property} to be ${value}, got ${response?.body[property]}`);
 		},
 	},
 	restResponseIs: {
-		gwta: 'response is {value}',
-		action: async ({ property, value }: TNamed) => {
+		gwta: 'http text response is {value}',
+		action: async ({ value }: TNamed) => {
 			const response = webPlaywright.getWorld().shared.get(LAST_REST_RESPONSE);
-			if (response && response.body && response.body[property]?.toString().includes(value)) {
+			if (response && response.text === value) {
 				return OK;
 			}
-			return actionNotOK(`Expected response.${property} to contain ${value}, got ${response?.body[property] || 'no response'}`);
+			return actionNotOK(`Expected response to be ${value}, got ${response?.text}`);
 		},
 	},
 });
+
+async function capturedResponse(response: Response) {
+	const capturedResponse = {
+		status: await response.status(),
+		statusText: await response.statusText(),
+		headers: response.headers,
+		url: response.url,
+		json: null,
+		text: null,
+	};
+
+	try {
+		capturedResponse.json = await response.json();
+	} catch (error) {
+		capturedResponse.text = await response.text();
+	}
+	return capturedResponse;
+}
