a2f623f5ef0f40fb7714cc94b0cf2edf6a64e7f4
vid <vid_github@zooid.org>
2025-03-17 07:54:15 -0700

    make step more specific

--- modules/core/src/steps/vars.test.ts
+++ modules/core/src/steps/vars.test.ts
@@ -8,26 +8,26 @@ describe('vars', () => {
 	it('assigns', async () => {
 		const feature = { path: '/features/test.feature', content: 'set "x" to "1"' };
 		const display = { path: '/features/display.feature', content: 'display "x"' };
-		const verify = { path: '/features/verify.feature', content: '"x" is "1"' };
+		const verify = { path: '/features/verify.feature', content: 'variable "x" is "1"' };
 		const res = await testWithDefaults([feature, display, verify], steppers);
 		expect(res.ok).toBe(true);
 	});
 	it('assigns empty', async () => {
 		const feature = { path: '/features/test.feature', content: 'set empty "x" to "y"' };
-		const verify = { path: '/features/verify.feature', content: '"x" is "y"' };
+		const verify = { path: '/features/verify.feature', content: 'variable "x" is "y"' };
 		const res = await testWithDefaults([feature, verify], steppers);
 		expect(res.ok).toBe(true);
 	});
 	it('empty does not overwrite', async () => {
 		const feature = { path: '/features/test.feature', content: 'set empty "x" to y' };
 		const notempty = { path: '/features/test.feature', content: 'set empty "x" to z' };
-		const verify = { path: '/features/verify.feature', content: '"x" is "y"' };
+		const verify = { path: '/features/verify.feature', content: 'variable "x" is "y"' };
 		const res = await testWithDefaults([feature, notempty, verify], steppers);
 		expect(res.ok).toBe(true);
 	});
 	it('is set', async () => {
 		const feature = { path: '/features/test.feature', content: 'set "x" to y' };
-		const verify = { path: '/features/verify.feature', content: '"x" is set' };
+		const verify = { path: '/features/verify.feature', content: 'variable "x" is set' };
 		const res = await testWithDefaults([feature, verify], steppers);
 		expect(res.ok).toBe(true);
 	});
