3cc750d16a08ffe2be04d7dbd1b9b548e8619ffe
vid <vid_github@zooid.org>
2025-03-17 12:22:18 -0700

    OAuth 2.0 tests

--- modules/web-playwright/src/rest-playwright.ts
+++ modules/web-playwright/src/rest-playwright.ts
@@ -8,42 +8,66 @@ const LAST_REST_RESPONSE = 'LAST_REST_RESPONSE';
 const PAYLOAD_METHODS = ['post', 'put', 'patch'];
 const NO_PAYLOAD_METHODS = ['get', 'delete', 'head'];
 
+export const AUTHORIZATION = 'Authorization';
+export const ACCESS_TOKEN = 'access_token';
+
+const HTTP = 'HTTP';
+
 export const restSteps = (webPlaywright: WebPlaywright) => ({
 	addAuthBearerToken: {
-		gwta: 'add auth bearer token {token}',
+		gwta: `add auth bearer token {token}`,
 		action: async ({ token }: TNamed) => {
 			const browserContext = await webPlaywright.getBrowserContext();
-			browserContext.setExtraHTTPHeaders({ Authorization: `Bearer ${token}` });
+			browserContext.setExtraHTTPHeaders({ [AUTHORIZATION]: `Bearer ${token}` });
+			return OK;
+		},
+	},
+	restTokenRequest: {
+		gwta: `request OAuth 2.0 access token from {endpoint}`,
+		action: async ({ endpoint }: TNamed) => {
+			const response = await webPlaywright.withPage<Response>(async (page: Page) => await page.request.get(endpoint));
+			const captured = await capturedResponse(response);
+			const accessToken = captured.json[ACCESS_TOKEN];
+			const browserContext = await webPlaywright.getBrowserContext();
+			browserContext.setExtraHTTPHeaders({ [AUTHORIZATION]: `Bearer ${accessToken}` });
+
+			webPlaywright.getWorld().shared.set(LAST_REST_RESPONSE, captured);
+
+			return OK;
+		},
+	},
+	restTokenLogout: {
+		gwta: `perform OAuth 2.0 logout from {endpoint}`,
+		action: async ({  endpoint }: TNamed) => {
+			const browserContext = await webPlaywright.getBrowserContext();
+			browserContext.setExtraHTTPHeaders({});
+
+			const response = await webPlaywright.withPage<Response>(async (page: Page) => await page.request.get(endpoint));
+			const captured = await capturedResponse(response);
+			webPlaywright.getWorld().shared.set(LAST_REST_RESPONSE, captured);
+
 			return OK;
 		},
 	},
 	restEndpointRequest: {
-		gwta: 'make an http {method} to {endpoint}',
+		gwta: `make an ${HTTP} {method} to {endpoint}`,
 		action: async ({ method, endpoint }: TNamed) => {
 			method = method.toLowerCase();
 			if (!NO_PAYLOAD_METHODS.includes(method)) {
 				return actionNotOK(`Method ${method} not supported`);
 			}
-			try {
-				const response = await webPlaywright.withPage<Response>(
-					async (page: Page) =>
-						await page.request[method](endpoint, {
-							// headers: {
-							// 	Authorization: `Bearer testtoken`,
-							// },
-						})
-				);
+			const cookies = await webPlaywright.getCookies();
+			const access_token = cookies.find((cookie) => cookie.name === ACCESS_TOKEN);
+			const browserContext = await webPlaywright.getBrowserContext();
+			const response = await webPlaywright.withPage<Response>(async (page: Page) => await page.request[method](endpoint));
 
-				webPlaywright.getWorld().shared.set(LAST_REST_RESPONSE, await capturedResponse(response));
+			webPlaywright.getWorld().shared.set(LAST_REST_RESPONSE, await capturedResponse(response));
 
-				return OK;
-			} catch (error: any) {
-				return actionNotOK(`REST request failed: ${error.message}`);
-			}
+			return OK;
 		},
 	},
 	restEndpointRequestWithPayload: {
-		gwta: 'make an http {method} to {endpoint} with {payload}',
+		gwta: `make an ${HTTP} {method} to {endpoint} with {payload}`,
 		action: async ({ method, endpoint, payload }: TNamed) => {
 			method = method.toLowerCase();
 			if (!PAYLOAD_METHODS.includes(method)) {
@@ -66,7 +90,7 @@ export const restSteps = (webPlaywright: WebPlaywright) => ({
 		},
 	},
 	restLastStatusIs: {
-		gwta: 'http status is {status}',
+		gwta: `${HTTP} status is {status}`,
 		action: async ({ status }: TNamed) => {
 			const response = webPlaywright.getWorld().shared.get(LAST_REST_RESPONSE);
 			if (response && response.status === parseInt(status)) {
@@ -76,17 +100,17 @@ export const restSteps = (webPlaywright: WebPlaywright) => ({
 		},
 	},
 	restResponsePropertyIs: {
-		gwta: 'http response property {property} is {value}',
+		gwta: `${HTTP} response property {property} is {value}`,
 		action: async ({ property, value }: TNamed) => {
 			const response = webPlaywright.getWorld().shared.get(LAST_REST_RESPONSE);
-			if (response && response.body && response.body[property]?.toString() === value) {
+			if (response && response.json && response.json[property] === value) {
 				return OK;
 			}
-			return actionNotOK(`Expected response.${property} to be ${value}, got ${response?.body[property]}`);
+			return actionNotOK(`Expected response.json.${property} to be ${value}, got ${JSON.stringify(response?.json[property])}`);
 		},
 	},
 	restResponseIs: {
-		gwta: 'http text response is {value}',
+		gwta: `${HTTP} text response is {value}`,
 		action: async ({ value }: TNamed) => {
 			const response = webPlaywright.getWorld().shared.get(LAST_REST_RESPONSE);
 			if (response && response.text === value) {
