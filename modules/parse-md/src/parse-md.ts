7df8e8f2a3d642a9776a6be3df6bcca59cb09719
vid <vid_github@zooid.org>
2025-04-02 08:06:59 -0700

    use bases

--- modules/parse-md/src/parse-md.ts
+++ modules/parse-md/src/parse-md.ts
@@ -5,19 +5,21 @@ import { parseMatches } from './parse.js';
 const conformance = /(?!\n|. )\b([A-Z].*? must .*?\.)/;
 
 const ParseMD = class ParseMD extends AStepper {
-  steps = {
-    conformance: {
-      gwta: `has annotated conformance doc from {where: string}`,
-      action: async ({ where }: TNamed) => {
-        try {
-          await parseMatches({ where: undefined }, this.getWorld().options.base as string, [conformance]);
-        } catch (e: any) {
-          return actionNotOK(e.message);
-        }
-        return OK;
-      },
-    },
-  };
+	steps = {
+		conformance: {
+			gwta: `has annotated conformance doc from {where: string}`,
+			action: async ({ where }: TNamed) => {
+				try {
+					for (const base of this.getWorld().bases) {
+						await parseMatches({ where: undefined }, base, [conformance]);
+					}
+				} catch (e: any) {
+					return actionNotOK(e.message);
+				}
+				return OK;
+			},
+		},
+	};
 };
 
 export default ParseMD;
