acac31986b49471ca6d130ed08c1d07569da269b
vid <vid_github@zooid.org>
2025-04-04 06:46:33 -0700

    refactoring monitor inline (wip)

--- vitest.config.ts
+++
@@ -1,8 +0,0 @@
-import { configDefaults, defineProject } from 'vitest/config'
-
-export default defineProject({
-  test: {
-    include: ['**/*.{test}.ts'],
-    exclude: [...configDefaults.exclude, '**/build/**']
-  }
-});
