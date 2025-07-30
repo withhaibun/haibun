7008a0cf2cfdb98297467a1189992519c3a0139e
vid <vid_github@zooid.org>
2025-07-30 15:32:29 -0400

    only accept colons for placeholders

--- modules/web-server-express/src/server-express.test.ts
+++ modules/web-server-express/src/server-express.test.ts
@@ -17,3 +17,19 @@ describe('mounts', () => {
 		void expect(async () => Promise.resolve(se.addRoute('get', '/', () => undefined))).rejects.toThrow();
 	});
 });
+
+
+describe('checkMOunted', () => {
+	it('throws on illegal characters', () => {
+		const tl = new TestLogger();
+		const se = new ServerExpress(tl, '/', 8999);
+		se.listen = async () => Promise.resolve('started');
+		expect(se.checkMountBadOrMounted('get', 'http://somewhere', 'test')).toMatch(/illegal characters/);
+	});
+	it('accepts placeholder', () => {
+		const tl = new TestLogger();
+		const se = new ServerExpress(tl, '/', 8999);
+		se.listen = async () => Promise.resolve('started');
+		expect(se.checkMountBadOrMounted('get', '/:id', 'test')).toBeUndefined();
+	});
+});
