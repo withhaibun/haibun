44179e206ff34ef30b0985e0199d8c020f53065d
vid <vid_github@zooid.org>
2025-10-09 14:54:41 -0700

    add accessibility domain

--- modules/mobile/src/domains.ts
+++ modules/mobile/src/domains.ts
@@ -2,6 +2,7 @@ import { TDomainDefinition, TStepValue } from '@haibun/core/lib/defs.js';
 import { DOMAIN_STRING } from '@haibun/core/lib/domain-types.js';
 
 export const MOBILE_TESTID = 'mobile-testid';
+export const MOBILE_ACCESSIBILITY = 'mobile-accessibility';
 export const MOBILE_XPATH = 'mobile-xpath';
 
 export const MobileDomains: TDomainDefinition[] = [
@@ -10,16 +11,23 @@ export const MobileDomains: TDomainDefinition[] = [
     coerce: (proto: TStepValue) => `~${String(proto.value)}`,
   },
   {
+    selectors: [MOBILE_ACCESSIBILITY],
+    coerce: (proto: TStepValue) => `~${String(proto.value)}`,
+  },
+  {
     selectors: [MOBILE_XPATH],
     coerce: (proto: TStepValue) => String(proto.value),
   },
   {
-    selectors: [MOBILE_TESTID, MOBILE_XPATH, DOMAIN_STRING],
+    selectors: [MOBILE_TESTID, MOBILE_ACCESSIBILITY, MOBILE_XPATH, DOMAIN_STRING],
     coerce: (proto: TStepValue) => {
       if (proto.domain === MOBILE_XPATH) {
         return String(proto.value);
       }
-      return `~${String(proto.value)}`;
+      if (proto.domain === MOBILE_TESTID || proto.domain === MOBILE_ACCESSIBILITY) {
+        return `~${String(proto.value)}`;
+      }
+      return String(proto.value);
     },
   },
 ];
