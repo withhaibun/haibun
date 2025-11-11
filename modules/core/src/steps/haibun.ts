8249c00b38e74374948fe0121df273ddcfd3a9a1
vid <vid_github@zooid.org>
2025-11-11 07:24:32 -0800

    correct haibun until

--- modules/core/src/steps/haibun.ts
+++ modules/core/src/steps/haibun.ts
@@ -82,12 +82,12 @@ class Haibun extends AStepper {
 
 		// --- META & UTILITIES ---
 		until: {
-			gwta: 'until {statements:DOMAIN_STATEMENT}',
+			gwta: `until {statements:${DOMAIN_STATEMENT}}`,
 			action: async ({ statements }: { statements: TFeatureStep[] }, featureStep: TFeatureStep) => {
 				let result: TStepResult;
 				do {
 					result = await executeSubFeatureSteps(featureStep, statements, this.steppers, this.getWorld(), ExecMode.WITH_CYCLES);
-					await sleep(500);
+					await sleep(200);
 				} while (!result.ok);
 				return OK;
 			},
