80ad6e27e1bc566e670b19a9a89a659229ae6ec0
vid <vid_github@zooid.org>
2026-01-15 08:04:35 -0500

    supress deprecated

--- modules/web-playwright/src/web-playwright.ts
+++ modules/web-playwright/src/web-playwright.ts
@@ -239,7 +239,8 @@ export class WebPlaywright extends AStepper implements IHasOptions, IHasCycles {
 
 	async captureAccessibilitySnapshot() {
 		return await this.withPage(async (page: Page) => {
-			const snapshot = await page.accessibility.snapshot({
+			// Note: page.accessibility is deprecated in Playwright. Consider migrating to @axe-core/playwright
+			const snapshot = await (page as TAnyFixme).accessibility.snapshot({
 				interestingOnly: false,
 			});
 			return snapshot;
