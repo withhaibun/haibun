87aeb97fec2bdd44c662c30a68000f18c5bda309
vid <vid_github@zooid.org>
2026-04-01 08:12:42 -0700

    feat: add utils version module

---
+++ modules/utils/src/version.ts
@@ -0,0 +1,183 @@
+#!/usr/bin/env node
+
+import { existsSync, readFileSync, writeFileSync } from 'fs';
+import { spawnCommand } from './util/index.js';
+import { createVitest } from 'vitest/node';
+
+const [, me, version, ...extra] = process.argv;
+
+class Versioner {
+	localAndExtraModules: { [name: string]: string } = {};
+	private noTest = false;
+	private noInstall = false;
+	private tag: string | undefined = undefined;
+	noPublish = false;
+
+	haibunPackageVersions: { [dep: string]: string } = {};
+
+	constructor(private version: string) {
+		if (!version) {
+			console.error(`usage: ${me}: <version> <extra modules> [--notest] [--tag=<tag>] [--nopublish]`);
+			process.exit(1);
+		}
+		for (let i = extra.length - 1; i >= 0; i--) {
+			const e = extra[i];
+			if (e.startsWith('--tag=')) {
+				this.tag = e.split('=')[1];
+				extra.splice(i, 1);
+			} else if (e === '--notest') {
+				this.noTest = true;
+				extra.splice(i, 1);
+			} else if (e === '--noinstall') {
+				this.noInstall = true;
+				extra.splice(i, 1);
+			} else if (e === '--nopublish') {
+				this.noPublish = true;
+				extra.splice(i, 1);
+			} else if (e.startsWith('-')) {
+				throw Error(`unknown option ${e}; use --tag=<tag>, --notest or --nopublish`);
+			}
+		}
+	}
+
+	async doVersion() {
+		const haibunPackageJson = this.updateHaibunPackageVersions();
+		haibunPackageJson.version = this.version;
+		writeFileSync('./package.json', JSON.stringify(haibunPackageJson, null, 2));
+
+		this.setLocalAndExtraModules();
+
+		await this.forLocalAndExtraModules(this.updateModule as (name: string, location: string) => Promise<void>);
+		this.updateSourceCurrentVersion();
+		await this.gitCommit('haibun', '.', ['./modules/core/src/currentVersion.ts']);
+		await this.forLocalAndExtraModules(this.npmInstall);
+		await this.forLocalAndExtraModules(this.runTest);
+		await this.forLocalAndExtraModules(this.gitCommit);
+		await this.forLocalAndExtraModules(this.npmPublish);
+		await this.forLocalAndExtraModules(this.gitPush);
+	}
+
+
+	async forLocalAndExtraModules(someFunction: (name: string, location: string) => Promise<void>) {
+		console.info(`\n## ${someFunction.name}`);
+		for (const [name, module] of Object.entries(this.localAndExtraModules)) {
+			console.info('running', someFunction.name, 'for', name, module);
+			await someFunction.call(this, name, module); // Bind `this` to each action
+		}
+	}
+
+	updateHaibunPackageVersions() {
+		const hpkg = JSON.parse(readFileSync('./package.json', 'utf-8'));
+		if (hpkg.name !== 'haibun') {
+			throw Error('not in haibun root');
+		}
+		for (const [dep, version] of Object.entries({ ...hpkg.dependencies, ...hpkg.devDependencies })) {
+			this.haibunPackageVersions[dep] = <string>version;
+		}
+		return hpkg;
+	}
+
+	private updateSourceCurrentVersion() {
+		for (const [dest, ext] of Object.entries({ src: 'ts', build: 'js' })) {
+			writeFileSync(`./modules/core/${dest}/currentVersion.${ext}`, `export const currentVersion = '${this.version}';\n`);
+			console.info('updated currentVersion', dest);
+		}
+	}
+
+	setLocalAndExtraModules() {
+		const modules = JSON.parse(readFileSync(`./modules/tsconfig.json`, 'utf-8'))
+			.references.map((f: { path: string }) => `./modules/${f.path}`)
+			.concat(extra);
+		for (const module of modules) {
+			const name = module.replace(/\/$/, '').replace(/.*\//, '');
+			this.localAndExtraModules[name] = module;
+		}
+	}
+
+	async npmPublish(name: string, module: string) {
+		const publish = ['npm', 'publish'];
+		if (this.tag) {
+			publish.push('--tag', this.tag);
+		}
+		if (this.noPublish) {
+			return;
+		}
+		await spawnCommand(publish, module).catch((e) => {
+			console.error(`npm publish failed for ${name}: ${e}`);
+		});
+	}
+
+	async gitPush(name: string, module: string) {
+		await spawnCommand(['git', 'push'], module).catch((e) => {
+			console.error(`git push failed for ${name}: ${e}`);
+		});
+	}
+
+	updateModule(name: string, location: string): Promise<void> {
+		const pkgFile = `${location}/package.json`;
+		const pkg = JSON.parse(readFileSync(pkgFile, 'utf-8'));
+		if (!pkg.publish && pkg.publish !== undefined) {
+			return;
+		}
+		const { main } = pkg;
+		if (main && !main.includes('*') && !existsSync(`${location}/${main}`)) {
+			throw Error(`main file ${main} does not exist in ${location}`);
+		}
+		pkg.version = this.version;
+		this.updateDependencies(pkg.dependencies);
+		this.updateDependencies(pkg.devDependencies);
+
+		writeFileSync(pkgFile, JSON.stringify(pkg, null, 2));
+		return Promise.resolve();
+	}
+
+	updateDependencies(dependencies: { [key: string]: string }) {
+		for (const d in dependencies) {
+			if (Object.hasOwn(dependencies, d)) {
+				if (d.startsWith('@haibun/')) {
+					dependencies[d] = this.version;
+				}
+				if (this.haibunPackageVersions[d]) {
+					dependencies[d] = this.haibunPackageVersions[d];
+				}
+			}
+		}
+	}
+
+	async gitCommit(name: string, location: string, extraPackages: string[] = []) {
+		const packages = [...extraPackages, 'package.json'];
+		await spawnCommand(['git', 'commit', '-m', `'update ${name} to version ${this.version}'`, ...packages], location).catch((e) => {
+			console.error(`git commit failed for ${name}: ${e}`);
+		});
+	}
+
+	async runTest(name: string, location: string) {
+		if (this.noTest) {
+			return;
+		}
+		const originalDir = process.cwd();
+		try {
+			process.chdir(location);
+
+			const vitest = await createVitest('test', { watch: false });
+			await vitest.start();
+			await vitest?.close();
+		} catch (error) {
+			console.error(`npm test failed for ${name}: ${error}`);
+			throw error;
+		} finally {
+			process.chdir(originalDir);
+		}
+	}
+
+	async npmInstall(name: string, location: string) {
+		if (this.noInstall) {
+			return;
+		}
+		await spawnCommand(['npm', 'install'], location).catch((e) => {
+			console.error(`npm install failed for ${name}: ${e}`);
+		});
+	}
+}
+
+void (await new Versioner(version).doVersion());
