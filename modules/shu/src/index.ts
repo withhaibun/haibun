8e1527fbbe4ba8da00fa5f50a8c09537087b08fd
vid <vid_github@zooid.org>
2026-04-09 15:56:51 -0700

    fix: use package paths in shu e2e test imports

--- modules/shu/src/index.ts
+++ modules/shu/src/index.ts
@@ -1,4 +1,3 @@
 export { default as ShuStepper } from "./shu-stepper.js";
 export { SHU_TEST_IDS } from "./test-ids.js";
 export { stepTestIds, createStepUI, flattenTestIds } from "./test/step-ui.js";
-export { buildMermaidSource, buildClassifier, DEFAULT_MAX_PER_SUBGRAPH, type PropertyClassifier, type TGraphViewOpts, type TBuildResult } from "./mermaid-source.js";
