fa9df30deba050bb187f96004434455304d78446
vid <vid_github@zooid.org>
2026-05-08 08:57:26 -0400

    fix: require code owner (admins team) approval for release branch PRs

--- scripts/protect-branches.sh
+++ scripts/protect-branches.sh
@@ -23,7 +23,7 @@ protect() {
   "enforce_admins": false,
   "required_pull_request_reviews": {
     "dismiss_stale_reviews": true,
-    "require_code_owner_reviews": false,
+    "require_code_owner_reviews": true,
     "required_approving_review_count": 1
   },
   "restrictions": null,
